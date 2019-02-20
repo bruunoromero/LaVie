61f2a3c2d85c96c36d825987ee09fcb8668d7f2e
Bruno Romero <bruunobromero@gmail.com>
2019-02-20 15:03:34 -0300

    Initial Commit

---
+++ LaVie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LaVie
+//
+//  Created by Bruno Barreira on 20/02/19.
+//  Copyright © 2019 Bruno Barreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
